f69d04bd18240b4c4ff488a755c5cc7d24b3369b
Hadayetullah <hadayetullah30@gmail.com>
2024-11-08 21:34:33 +0600

    Create two api endpoits to upload and list data

--- backend/app_files/urls.py
+++ backend/app_files/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import FileUploadView, FileListView
 
 urlpatterns = [
-   
+   path('upload/', FileUploadView.as_view(), name='api_upload'),
+   path('files/list/', FileListView.as_view(), name='api_files_list'),
 ]
--- backend/app_files/views.py
+++ backend/app_files/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import FileUpload
+from .serializers import FileUploadSerializer
 
-# Create your views here.
+class FileUploadView(APIView):
+    def post(self, request):
+        files = request.FILES.getlist('files')
+       
+        # Process each uploaded file
+        for file in files:
+            FileUpload.objects.create(file=file, name=file.name)
+            
+        # Get all files from the database
+        all_files = FileUpload.objects.all()
+        all_files_serializer = FileUploadSerializer(all_files, many=True, context={'request': request})
+
+        return Response({"all_files": all_files_serializer.data}, status=status.HTTP_201_CREATED)
+    
+
+
+class FileListView(APIView):
+    def get(self, request):
+        all_files = FileUpload.objects.all()
+        all_files_serializer = FileUploadSerializer(all_files, many=True, context={'request': request})
+
+        return Response({"all_files": all_files_serializer.data}, status=status.HTTP_200_OK)
+
+    
+    
